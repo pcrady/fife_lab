a941f28209c559cc9503954b223b32a0bc098b88
Peter Crady <petercrady1@gmail.com>
2025-04-28 22:13:19 -0500

    finished about

--- lib/widgets/dropdown_buttons/title_dropdown.dart
+++ lib/widgets/dropdown_buttons/title_dropdown.dart
@@ -2,6 +2,7 @@ import 'package:fife_lab/widgets/dropdown_buttons/app_bar_dropdown.dart';
 import 'package:fife_lab/widgets/fife_lab_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 enum _Title {
   about('About'),
@@ -28,17 +29,36 @@ class _TitleDropdownState extends ConsumerState<TitleDropdown> {
   void onSelected(_Title choice) {
     switch (choice) {
       case _Title.about:
-        FifeLabDialog.showDialogWrapper(
-          title: 'About',
-          content: Text('Some content'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('Close'),
+        {
+          FifeLabDialog.showDialogWrapper(
+            title: 'About',
+            content: FutureBuilder(
+              future: PackageInfo.fromPlatform(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Container();
+                }
+                final packageInfo = snapshot.data;
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('App Name: ${packageInfo?.appName ?? 'Error'}'),
+                    Text('Version: ${packageInfo?.version ?? 'Error'}'),
+                    Text('Build Number: ${packageInfo?.buildNumber ?? 'Error'}'),
+                  ],
+                );
+              }
             ),
-          ],
-          context: context,
-        );
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Close'),
+              ),
+            ],
+            context: context,
+          );
+        }
       case _Title.preferences:
         FifeLabDialog.showDialogWrapper(
           title: 'Preferences',
