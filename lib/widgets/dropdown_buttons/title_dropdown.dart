596a1e309582298ef9e2f4f96ef8cef8d233e34e
Peter Crady <petercrady1@gmail.com>
2025-04-28 23:38:09 -0500

    can view log files now

--- lib/widgets/dropdown_buttons/title_dropdown.dart
+++ lib/widgets/dropdown_buttons/title_dropdown.dart
@@ -1,3 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:fife_lab/lib/app_logger.dart';
+import 'package:fife_lab/lib/initializer.dart';
 import 'package:fife_lab/widgets/dropdown_buttons/app_bar_dropdown.dart';
 import 'package:fife_lab/widgets/fife_lab_dialog.dart';
 import 'package:flutter/material.dart';
@@ -32,24 +38,7 @@ class _TitleDropdownState extends ConsumerState<TitleDropdown> {
         {
           FifeLabDialog.showDialogWrapper(
             title: 'About',
-            content: FutureBuilder(
-              future: PackageInfo.fromPlatform(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Container();
-                }
-                final packageInfo = snapshot.data;
-                return Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('App Name: ${packageInfo?.appName ?? 'Error'}'),
-                    Text('Version: ${packageInfo?.version ?? 'Error'}'),
-                    Text('Build Number: ${packageInfo?.buildNumber ?? 'Error'}'),
-                  ],
-                );
-              }
-            ),
+            content: _AboutContent(),
             actions: <Widget>[
               TextButton(
                 onPressed: () => Navigator.of(context).pop(),
@@ -62,7 +51,7 @@ class _TitleDropdownState extends ConsumerState<TitleDropdown> {
       case _Title.preferences:
         FifeLabDialog.showDialogWrapper(
           title: 'Preferences',
-          content: Text('Some content'),
+          content: Text('asdf'),
           actions: <Widget>[
             TextButton(
               onPressed: () => Navigator.of(context).pop(),
@@ -74,7 +63,7 @@ class _TitleDropdownState extends ConsumerState<TitleDropdown> {
       case _Title.serverLogs:
         FifeLabDialog.showDialogWrapper(
           title: 'Server Logs',
-          content: Text('Some content'),
+          content: _ServerLogsContent(),
           actions: <Widget>[
             TextButton(
               onPressed: () => Navigator.of(context).pop(),
@@ -105,6 +94,160 @@ class _TitleDropdownState extends ConsumerState<TitleDropdown> {
       onSelected: onSelected,
       values: _Title.values,
       title: 'Fife Lab',
+    );
+  }
+}
+
+class _AboutContent extends StatelessWidget {
+  const _AboutContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: PackageInfo.fromPlatform(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Container();
+        }
+        final packageInfo = snapshot.data;
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('App Name: ${packageInfo?.appName ?? 'Error'}'),
+            Text('Version: ${packageInfo?.version ?? 'Error'}'),
+            Text('Build Number: ${packageInfo?.buildNumber ?? 'Error'}'),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _ServerLogsContent extends StatefulWidget {
+  const _ServerLogsContent({super.key});
+
+  @override
+  State<_ServerLogsContent> createState() => _ServerLogsContentState();
+}
+
+class _ServerLogsContentState extends State<_ServerLogsContent> {
+  late String selectedFilePath;
+  List<String> logFileContents = [];
+  late final StreamController<String> _logStreamController;
+  late final ScrollController scrollController;
+  bool serverLog = true;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController();
+    _logStreamController = StreamController();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setScroll());
+    _startReadingFile();
+  }
+
+  //TODO this is janky
+  Future<void> setScroll() async {
+    if (scrollController.hasClients) {
+      scrollController.jumpTo(scrollController.position.maxScrollExtent);
+    } else {
+      await Future.delayed(Duration(milliseconds: 50));
+      setScroll();
+    }
+  }
+
+  String stripAnsiCodes(String text) {
+    final ansiRegex = RegExp(r'\x1B\[[0-?]*[ -/]*[@-~]');
+    return text.replaceAll(ansiRegex, '');
+  }
+
+  void _startReadingFile() async {
+    final file = File(Initializer.serverLogPath!);
+
+    if (await file.exists()) {
+      final stream = file.openRead();
+      try {
+        await for (final line in stream.transform(utf8.decoder).transform(const LineSplitter())) {
+          final strippedLine = stripAnsiCodes(line);
+          _logStreamController.add(strippedLine);
+          logFileContents.add(strippedLine);
+        }
+      } catch (e) {
+        _logStreamController.addError("Error reading file: $e");
+        _logStreamController.close();
+      }
+    } else {
+      _logStreamController.addError("File does not exist.");
+      _logStreamController.close();
+    }
+  }
+
+  @override
+  void dispose() {
+    _logStreamController.close();
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 1200,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SelectionArea(
+            child: Text('Server Log File At: ${Initializer.serverLogPath}'),
+          ),
+          const Divider(),
+          SizedBox(height: 8.0),
+          StreamBuilder<String>(
+            stream: _logStreamController.stream,
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (!snapshot.hasData) {
+                return const Text('No data');
+              }
+
+              return SizedBox(
+                height: 700,
+                child: RawScrollbar(
+                  thumbColor: Colors.white30,
+                  radius: const Radius.circular(20),
+                  controller: scrollController,
+                  child: ListView.builder(
+                    controller: scrollController,
+                    shrinkWrap: true,
+                    itemCount: logFileContents.length,
+                    itemBuilder: (context, index) {
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: SelectionArea(
+                              child: Text(
+                                logFileContents[index],
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'monospace', // 👈 fixed-width font
+                                  // Optional: fallbacks for older systems
+                                  fontFamilyFallback: ['Courier', 'Courier New'],
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
