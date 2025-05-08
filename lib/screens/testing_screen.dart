fef5632a6d194d8caa050f7e52310b1731c21197
Peter Crady <petercrady1@gmail.com>
2025-05-08 11:08:33 -0500

    updates

--- lib/screens/testing_screen.dart
+++ lib/screens/testing_screen.dart
@@ -28,7 +28,7 @@ class _TestingScreenState extends ConsumerState<TestingScreen> {
     try {
       final dio = Dio(BaseOptions(baseUrl: kServer));
       List<Future> futures = [];
-      for (int i = 0; i < 10000; i++) {
+      for (int i = 0; i < 1000; i++) {
         futures.add(dio.post(
           '/iotest',
           data: {'test_type': 'rapid_io_test', 'value': 1},
